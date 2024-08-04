f23e345665ee68ec211550f4771d35dfff048122
ibragimov05 <ibragimov.asus15@gmail.com>
2024-08-04 20:11:20 +0500

    debug banner has been added

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -24,7 +24,7 @@ class _SummarizeItState extends State<SummarizeIt> {
       builder: (context, state) {
         return ToastificationWrapper(
           child: MaterialApp(
-            debugShowCheckedModeBanner: false,
+            debugShowCheckedModeBanner: true,
             theme: ThemeData(
               textSelectionTheme: TextSelectionThemeData(
                 cursorColor: AppColors.green900,
