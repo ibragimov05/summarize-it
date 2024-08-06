a09ee58820ec48d7d37ca1733d2a304eeed4a6e0
ibragimov05 <ibragimov.asus15@gmail.com>
2024-08-06 10:26:16 +0500

    some changes have been added

--- lib/logic/blocs/monitor_connection/monitor_connection_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'monitor_connection_bloc.dart';
-
-@immutable
-sealed class MonitorConnectionEvent {}
--- lib/logic/blocs/monitor_connection/monitor_connection_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'monitor_connection_bloc.dart';
-
-@immutable
-sealed class MonitorConnectionState {}
-
-final class MonitorConnectionInitial extends MonitorConnectionState {}
